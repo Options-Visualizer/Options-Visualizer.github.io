63ab33b8f51bc4c4a06011d4651957ef1f577613
zmoustafa <zeyadmoustafa10@gmail.com>
2020-07-22 22:15:19 -0400

    Initialized React app and published to Github Pages
    + Added favicon

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          Options Visualizer        
+        </div>
+        <img src={process.env.PUBLIC_URL + '/favicon.ico'} alt="Logo"></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
