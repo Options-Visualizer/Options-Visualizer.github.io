ba2a2ef276fd7382b2bea45cf05125fe2652e6d5
zmoustafa <zeyadmoustafa10@gmail.com>
2020-07-24 21:09:11 -0400

    Updates

---
+++ precache-manifest.c3f2c3cc4435356430551d8cab3ad416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836bd4c6716a85f9968c0d57d26a3c80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ef78b395fd475e5e098",
+    "url": "/static/css/main.459e4439.chunk.css"
+  },
+  {
+    "revision": "b2ac3d33b89e0b90364a",
+    "url": "/static/js/2.ad9c2285.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.ad9c2285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ef78b395fd475e5e098",
+    "url": "/static/js/main.d0dc3e5f.chunk.js"
+  },
+  {
+    "revision": "a8f5c78092a82cf6f7de",
+    "url": "/static/js/runtime-main.6035b029.js"
+  }
+]);
