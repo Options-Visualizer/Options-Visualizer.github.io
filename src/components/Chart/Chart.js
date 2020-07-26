43acf632126e10a64eb4a618ce960a6728477123
zmoustafa <zeyadmoustafa10@gmail.com>
2020-07-26 19:54:53 -0400

    Incorporated redux for managing ticker state

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Chart.css';
 import {Line} from 'react-chartjs-2';
 import 'chartjs-plugin-annotation';
+import { connect } from 'react-redux';
 
 
-function Chart(info) {
+function Chart (props) {
   let chartLabels = []; 
   let verticalLines = [];
   let pldata = []
   let dataLabel = "";
+  const [quote, setQuote] = useState(0);
+  const [high, setHigh] = useState(0); 
+  const [low, setLow] = useState(0);
 
-  if (info.high !== 0){
-    const intQuote = Math.floor(info.quote / 5) * 5;
-    let third = (info.quote - intQuote) * 100 - 150; 
-    if (info.quote < intQuote) { 
+
+
+
+  useEffect(() => {
+    async function getTickerData(tick) {
+      console.log(tick);
+      let response = await fetch('https://cloud.iexapis.com/stable/stock/' + tick + '/quote?token=' + process.env.REACT_APP_IEX_API_KEY);
+      let data = await response.json();
+      return data;
+    }
+    if (props.ticker.ticker !== undefined ){
+      getTickerData(props.ticker.ticker).then(data => {
+        setHigh(data.week52High);
+        setLow(data.week52Low);
+        setQuote(data.latestPrice);
+      });
+    }
+  }, [props.ticker]);
+  
+  if (high !== 0){
+    const intQuote = Math.floor(quote / 5) * 5;
+    let third = (quote - intQuote) * 100 - 150; 
+    if (quote < intQuote) { 
       third = -150;
     }
-    pldata = [-150, -150, third, (info.high - intQuote) * 100 - 150, 2 * (info.high - intQuote) * 100 - 150]
+    pldata = [-150, -150, third, (high - intQuote) * 100 - 150, 2 * (high - intQuote) * 100 - 150]
     dataLabel = "Long " + intQuote + " call @ 1.50";
-    chartLabels = [0, info.low, info.quote, info.high, info.high + info.high - info.quote];
+    chartLabels = [0, low, quote, high, high + high - quote];
     verticalLines = [
       {
         type: "line",
@@ -84,7 +107,7 @@ function Chart(info) {
         options={{
           title:{
             display:true,
-            text: info.ticker + " Profit/Loss Chart",
+            text: (props.ticker.ticker === undefined ? "" : props.ticker.ticker) + " Profit/Loss Chart",
             fontSize:20
           },
           legend:{
@@ -127,4 +150,10 @@ function Chart(info) {
   );
 }
 
-export default Chart;
+const mapStateToProps = (state) => ({
+  ticker: state.ticker
+});
+
+export default connect(
+  mapStateToProps, 
+)(Chart);
