016c0b06bb99e17ec08ec9ff0099bf7a32b229a7
geomaster453 <austin.wei@lakesideschool.org>
2020-07-26 22:44:29 -0700

    Updates

---
+++ precache-manifest.adfa78527aa93450acfb5e362344ad57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1660d296d51bd58798a5c7a4b27b378d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "553571fbd0a11edcd29b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ccd8524b3bbd7314d6a5",
+    "url": "/static/css/main.d6490fbc.chunk.css"
+  },
+  {
+    "revision": "553571fbd0a11edcd29b",
+    "url": "/static/js/2.cd95f8a0.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/static/js/2.cd95f8a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd8524b3bbd7314d6a5",
+    "url": "/static/js/main.e1dcb327.chunk.js"
+  },
+  {
+    "revision": "a8f5c78092a82cf6f7de",
+    "url": "/static/js/runtime-main.6035b029.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1e7f13030c61b0d5bd2aba3ca84c349b.js"
+  "/precache-manifest.adfa78527aa93450acfb5e362344ad57.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
