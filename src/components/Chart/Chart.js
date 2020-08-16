805ad4795162a35a119771f6bbd0cd6f194f8b80
zmoustafa <zeyadmoustafa10@gmail.com>
2020-08-16 18:53:58 -0400

    Modified charts

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,33 +1,63 @@
 import React, { useState } from 'react';
 import './Chart.css';
-import {Line} from 'react-chartjs-2';
+import {Line, Scatter} from 'react-chartjs-2';
 import 'chartjs-plugin-annotation';
 import { useSelector } from 'react-redux'
 
 function Chart (props) {
-  let chartLabels = []; 
   let verticalLines = [];
   let pldata = []
   let dataLabel = "";
   const [quote, setQuote] = useState(0);
   const [high, setHigh] = useState(0); 
   const [low, setLow] = useState(0);
+  const [chartData, setChartData] = useState([]);
   const symbol = useSelector(state => state.currentSymbol)
 
+<<<<<<< HEAD
   /*useEffect(() => {
+=======
+  let premium = 10; 
+  let strike = quote;
+  let strategy = "Short"
+
+  let factor = -1; 
+  if (strategy === "Short"){ 
+    factor = 1;
+  }
+  
+  useEffect(() => {
+
+>>>>>>> Modified charts
     async function getTickerData(tick) {
       console.log(tick);
       let response = await fetch('https://cloud.iexapis.com/stable/stock/' + tick + '/quote?token=' + process.env.REACT_APP_IEX_API_KEY);
       let data = await response.json();
       return data;
     }
-    if (symbol !== undefined ){
+    if (symbol !== undefined && symbol !== ''){
       getTickerData(symbol).then(data => {
+        console.log(data);
+        let chart_data = []; 
+        let cur_x = Math.ceil(data.week52Low - data.latestPrice/10 - 10);
+        while (cur_x <= Math.ceil(data.week52High + data.latestPrice/10 + 10)){
+          let cur_y = factor * premium;
+
+          if (cur_x - data.latestPrice >= premium){
+            cur_y = - factor * (cur_x - data.latestPrice - premium);
+          }
+          
+          chart_data.push({x: Number(cur_x).toFixed(2), y: Number(cur_y).toFixed(2)});
+          cur_x += 10;
+        }
+        console.log(chart_data);
         setHigh(data.week52High);
         setLow(data.week52Low);
         setQuote(data.latestPrice);
+        setChartData(chart_data);
       });
     }
+<<<<<<< HEAD
   }, [symbol]);*/
 
   async function getTickerData(tick) {
@@ -45,26 +75,33 @@ function Chart (props) {
     });
   }
 
+=======
+
+  }, [symbol]);
+  
+>>>>>>> Modified charts
   if (high !== 0){
     const intQuote = Math.floor(quote / 5) * 5;
     let third = (quote - intQuote) * 100 - 150; 
     if (quote < intQuote) { 
       third = -150;
     }
-    pldata = [-150, -150, third, (high - intQuote) * 100 - 150, 2 * (high - intQuote) * 100 - 150]
+    pldata = [1, 2,,4];
     dataLabel = "Long " + intQuote + " call @ 1.50";
-    chartLabels = [0, low, quote, high, high + high - quote];
     verticalLines = [
       {
         type: "line",
         mode: "vertical",
         scaleID: "x-axis-0",
-        value: 1,
+        value: Number(low).toFixed(2),
         borderColor: "#38677D",
         borderWidth: 2,
+        borderDash: [2],
+
         label: {
           backgroundColor: "#959296",
           content: "52 wk Low",
+          position: "bottom",
           enabled: true
         }
       },
@@ -72,9 +109,11 @@ function Chart (props) {
         type: "line",
         mode: "vertical",
         scaleID: "x-axis-0",
-        value: 2,
+        value: Number(quote).toFixed(2),
         borderColor: "#38677D",
         borderWidth: 2,
+        borderDash: [2],
+
         label: {
           backgroundColor: "#959296",
           content: "Current Price",
@@ -85,29 +124,30 @@ function Chart (props) {
         type: "line",
         mode: "vertical",
         scaleID: "x-axis-0",
-        value: 3,
+        value: Number(high).toFixed(2),
         borderColor: "#38677D",
         borderWidth: 2,
+        borderDash: [2],
+
         label: {
           backgroundColor: "#959296",
           content: "52 wk High",
+          position: "top",
           enabled: true
         }
       }      
     ];
   }
   const state = {
-    labels: chartLabels,
     datasets: [
       {
-        
         label: dataLabel,
         fill: false,
         lineTension: 0,
         backgroundColor: 'rgba(75,192,192,1)',
         borderColor: 'rgba(0,0,0,1)',
         borderWidth: 2,
-        data: pldata,
+        data: chartData
       }
     ]
   }
@@ -140,6 +180,7 @@ function Chart (props) {
               }
             }],
             xAxes: [{
+              type: 'linear',
               scaleLabel: {
                 display: true, 
                 labelString: 'Price at Expiration',
@@ -148,7 +189,9 @@ function Chart (props) {
               ticks: {
                 callback: function(value, index, values) { 
                   return '$' + value;
-                }
+                }, 
+                min: chartData[0] == undefined ? 0 :  Math.floor(chartData[0].x), 
+                max: chartData[chartData.length -1] == undefined ? 1 : Math.floor(chartData[chartData.length -1].x),
               }
             }]
           }, 
